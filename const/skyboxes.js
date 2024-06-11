55e88c7a19bf0617231d7cfd3618b1f18262c800
zaxel <sae11.z.alex@gmail.com>
2024-06-11 23:11:46 +0300

    Move skybox images to public folder to include them in Vite build.

--- const/skyboxes.js
+++ const/skyboxes.js
@@ -1,86 +1,86 @@
-﻿const arch = ['./assets/skybox/arch3_ft.png',
-	            './assets/skybox/arch3_bk.png',
-	            './assets/skybox/arch3_up.png',
-	            './assets/skybox/arch3_dn.png',
-	            './assets/skybox/arch3_rt.png',
-	            './assets/skybox/arch3_lf.png',];
+﻿const arch = ['./skybox/arch3_ft.png',
+	            './skybox/arch3_bk.png',
+	            './skybox/arch3_up.png',
+	            './skybox/arch3_dn.png',
+	            './skybox/arch3_rt.png',
+	            './skybox/arch3_lf.png',];
 
-const cave = ['./assets/skybox/cave3_ft.png',
-	'./assets/skybox/cave3_bk.png',
-	'./assets/skybox/cave3_up.png',
-	'./assets/skybox/cave3_dn.png',
-	'./assets/skybox/cave3_rt.png',
-	'./assets/skybox/cave3_lf.png',]; 
+const cave = ['./skybox/cave3_ft.png',
+	'./skybox/cave3_bk.png',
+	'./skybox/cave3_up.png',
+	'./skybox/cave3_dn.png',
+	'./skybox/cave3_rt.png',
+	'./skybox/cave3_lf.png',]; 
 
-const dark = ['./assets/skybox/dark_ft.png',
-	'./assets/skybox/dark_bk.png',
-	'./assets/skybox/dark_up.png',
-	'./assets/skybox/dark_dn.png',
-	'./assets/skybox/dark_rt.png',
-	'./assets/skybox/dark_lf.png',]; 
+const dark = ['./skybox/dark_ft.png',
+	'./skybox/dark_bk.png',
+	'./skybox/dark_up.png',
+	'./skybox/dark_dn.png',
+	'./skybox/dark_rt.png',
+	'./skybox/dark_lf.png',]; 
 
-const hot = ['./assets/skybox/hot_ft.png',
-	'./assets/skybox/hot_bk.png',
-	'./assets/skybox/hot_up.png',
-	'./assets/skybox/hot_dn.png',
-	'./assets/skybox/hot_rt.png',
-	'./assets/skybox/hot_lf.png',]; 
+const hot = ['./skybox/hot_ft.png',
+	'./skybox/hot_bk.png',
+	'./skybox/hot_up.png',
+	'./skybox/hot_dn.png',
+	'./skybox/hot_rt.png',
+	'./skybox/hot_lf.png',]; 
 
-const rainbow = ['./assets/skybox/rainbow_ft.png',
-	'./assets/skybox/rainbow_bk.png',
-	'./assets/skybox/rainbow_up.png',
-	'./assets/skybox/rainbow_dn.png',
-	'./assets/skybox/rainbow_rt.png',
-	'./assets/skybox/rainbow_lf.png',]; 
+const rainbow = ['./skybox/rainbow_ft.png',
+	'./skybox/rainbow_bk.png',
+	'./skybox/rainbow_up.png',
+	'./skybox/rainbow_dn.png',
+	'./skybox/rainbow_rt.png',
+	'./skybox/rainbow_lf.png',]; 
 
-const sh = ['./assets/skybox/sh_ft.png',
-	'./assets/skybox/sh_bk.png',
-	'./assets/skybox/sh_up.png',
-	'./assets/skybox/sh_dn.png',
-	'./assets/skybox/sh_rt.png',
-	'./assets/skybox/sh_lf.png',]; 
+const sh = ['./skybox/sh_ft.png',
+	'./skybox/sh_bk.png',
+	'./skybox/sh_up.png',
+	'./skybox/sh_dn.png',
+	'./skybox/sh_rt.png',
+	'./skybox/sh_lf.png',]; 
 
-const skyast = ['./assets/skybox/skyast_ft.png',
-	'./assets/skybox/skyast_bk.png',
-	'./assets/skybox/skyast_up.png',
-	'./assets/skybox/skyast_dn.png',
-	'./assets/skybox/skyast_rt.png',
-	'./assets/skybox/skyast_lf.png',]; 
+const skyast = ['./skybox/skyast_ft.png',
+	'./skybox/skyast_bk.png',
+	'./skybox/skyast_up.png',
+	'./skybox/skyast_dn.png',
+	'./skybox/skyast_rt.png',
+	'./skybox/skyast_lf.png',]; 
 
-const skyh = ['./assets/skybox/skyhsky_ft.png',
-	'./assets/skybox/skyhsky_bk.png',
-	'./assets/skybox/skyhsky_up.png',
-	'./assets/skybox/skyhsky_dn.png',
-	'./assets/skybox/skyhsky_rt.png',
-	'./assets/skybox/skyhsky_lf.png',]; 
+const skyh = ['./skybox/skyhsky_ft.png',
+	'./skybox/skyhsky_bk.png',
+	'./skybox/skyhsky_up.png',
+	'./skybox/skyhsky_dn.png',
+	'./skybox/skyhsky_rt.png',
+	'./skybox/skyhsky_lf.png',]; 
 
-const skype = ['./assets/skybox/skype_ft.png',
-	'./assets/skybox/skype_bk.png',
-	'./assets/skybox/skype_up.png',
-	'./assets/skybox/skype_dn.png',
-	'./assets/skybox/skype_rt.png',
-	'./assets/skybox/skype_lf.png',]; 
+const skype = ['./skybox/skype_ft.png',
+	'./skybox/skype_bk.png',
+	'./skybox/skype_up.png',
+	'./skybox/skype_dn.png',
+	'./skybox/skype_rt.png',
+	'./skybox/skype_lf.png',]; 
 
-const sp2 = ['./assets/skybox/sp2_ft.png',
-	'./assets/skybox/sp2_bk.png',
-	'./assets/skybox/sp2_up.png',
-	'./assets/skybox/sp2_dn.png',
-	'./assets/skybox/sp2_rt.png',
-	'./assets/skybox/sp2_lf.png',]; 
+const sp2 = ['./skybox/sp2_ft.png',
+	'./skybox/sp2_bk.png',
+	'./skybox/sp2_up.png',
+	'./skybox/sp2_dn.png',
+	'./skybox/sp2_rt.png',
+	'./skybox/sp2_lf.png',]; 
 
-const sp3 = ['./assets/skybox/sp3_ft.png',
-	'./assets/skybox/sp3_bk.png',
-	'./assets/skybox/sp3_up.png',
-	'./assets/skybox/sp3_dn.png',
-	'./assets/skybox/sp3_rt.png',
-	'./assets/skybox/sp3_lf.png',]; 
+const sp3 = ['./skybox/sp3_ft.png',
+	'./skybox/sp3_bk.png',
+	'./skybox/sp3_up.png',
+	'./skybox/sp3_dn.png',
+	'./skybox/sp3_rt.png',
+	'./skybox/sp3_lf.png',]; 
 
-const tron = ['./assets/skybox/sh_tron.png',
-	'./assets/skybox/sh_tron.png',
-	'./assets/skybox/sh_tron.png',
-	'./assets/skybox/sh_tron.png',
-	'./assets/skybox/sh_tron.png',
-	'./assets/skybox/sh_tron.png',];
+const tron = ['./skybox/sh_tron.png',
+	'./skybox/sh_tron.png',
+	'./skybox/sh_tron.png',
+	'./skybox/sh_tron.png',
+	'./skybox/sh_tron.png',
+	'./skybox/sh_tron.png',];
 
     export const skyboxesList = {
     arch, cave, dark, hot, rainbow, sh, skyast, skyh, skype, sp2, sp3, tron
