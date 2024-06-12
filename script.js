6b8e932d8fd259b37f7ff7987caefaca9bcf0595
zaxel <sae11.z.alex@gmail.com>
2024-06-12 14:10:40 +0300

    Switch to classes and create InitializeAnimationDemo class

--- script.js
+++ script.js
@@ -4,66 +4,80 @@ import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { FBXLoader } from 'three/addons/loaders/FBXLoader.js';
 import {skyboxesList} from './const/skyboxes.js';
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
-const renderer = new THREE.WebGLRenderer({
-	antialias: true,
-});
-renderer.shadowMap.enabled = true;
-renderer.shadowMap.type = THREE.PCFSoftShadowMap;
-renderer.setSize( window.innerWidth, window.innerHeight );
-renderer.setAnimationLoop( animate );
-document.body.appendChild( renderer.domElement );
-
-camera.position.set(75, 20, 0);
 
 
-const loader = new THREE.CubeTextureLoader();
-
-const texture = loader.load(skyboxesList.sh);
-scene.background = texture;
-
-
-const controls = new OrbitControls(camera, renderer.domElement);
-controls.target.set(0, 20, 0);
-controls.update();
-
-
-let dirLight = new THREE.DirectionalLight(0xFFFFFF, 0.6);
-dirLight.position.set(20, 100, 10);
-dirLight.target.position.set(0, 0, 0);
-dirLight.castShadow = true;
-dirLight.shadow.bias = -0.001;
-dirLight.shadow.mapSize.width = 2048;
-dirLight.shadow.mapSize.height = 2048;
-dirLight.shadow.camera.near = 0.1;
-dirLight.shadow.camera.far = 500.0;
-dirLight.shadow.camera.near = 0.5;
-dirLight.shadow.camera.far = 500.0;
-dirLight.shadow.camera.left = 100;
-dirLight.shadow.camera.right = -100;
-dirLight.shadow.camera.top = 100;
-dirLight.shadow.camera.bottom = -100;
-
-let amLight = new THREE.AmbientLight(0x101010);
-
-const plane = new THREE.Mesh(
-	new THREE.PlaneGeometry(100, 100, 10, 10),
-	new THREE.MeshStandardMaterial({
-			color: 0xFFFFFF,
+class InitializeAnimationDemo{
+	constructor(){
+		this._initialize();
+	}
+	_initialize(){
+		this._scene = new THREE.Scene();
+		this._camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+		
+		this._renderer = new THREE.WebGLRenderer({
+			antialias: true,
+		});
+		this._renderer.shadowMap.enabled = true;
+		this._renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+		this._renderer.setSize( window.innerWidth, window.innerHeight );
+		this._renderer.setAnimationLoop( () => this._animate() );
+		document.body.appendChild( this._renderer.domElement );
+		
+		this._camera.position.set(75, 20, 0);
+		
+		
+		const loader = new THREE.CubeTextureLoader();
+		
+		const texture = loader.load(skyboxesList.sh);
+		texture.encoding = THREE.sRGBEncoding;
+		this._scene.background = texture;
+		
+		
+		const controls = new OrbitControls(this._camera, this._renderer.domElement);
+		controls.target.set(0, 20, 0);
+		controls.update();
+		
+		
+		let dirLight = new THREE.DirectionalLight(0xFFFFFF, 0.6);
+		dirLight.position.set(20, 100, 10);
+		dirLight.target.position.set(0, 0, 0);
+		dirLight.castShadow = true;
+		dirLight.shadow.bias = -0.001;
+		dirLight.shadow.mapSize.width = 2048;
+		dirLight.shadow.mapSize.height = 2048;
+		dirLight.shadow.camera.near = 0.1;
+		dirLight.shadow.camera.far = 500.0;
+		dirLight.shadow.camera.near = 0.5;
+		dirLight.shadow.camera.far = 500.0;
+		dirLight.shadow.camera.left = 100;
+		dirLight.shadow.camera.right = -100;
+		dirLight.shadow.camera.top = 100;
+		dirLight.shadow.camera.bottom = -100;
+		
+		let amLight = new THREE.AmbientLight(0x101010);
+		
+		const plane = new THREE.Mesh(
+			new THREE.PlaneGeometry(100, 100, 10, 10),
+			new THREE.MeshStandardMaterial({
+					color: 0xFFFFFF,
 		}));
-	plane.castShadow = false;
-	plane.receiveShadow = true;
-	plane.rotation.x = -Math.PI / 2;
 
-scene.add(plane);
+		plane.castShadow = false;
+		plane.receiveShadow = true;
+		plane.rotation.x = -Math.PI / 2;
+	
+		this._scene.add(plane);
+	
+		this._scene.add(dirLight);
+		this._scene.add(amLight);
+	}
+	_animate(){
+		this._renderer.render( this._scene, this._camera );
+	}
+}
 
-scene.add(dirLight);
-scene.add(amLight);
 
 
-let mixer;
 
 function loadAnimatedModel(){
 	const mainHeroLoader = new FBXLoader();
@@ -94,12 +108,8 @@ function loadAnimatedModel(){
 		} 
 	);
 }
-loadAnimatedModel()
+// loadAnimatedModel()
 
-function animate() {
-	const delta = clock.getDelta();  
-    if (mixer) mixer.update(delta);  
-	renderer.render( scene, camera );
-}
 
-const clock = new THREE.Clock();
+
+const _App = new InitializeAnimationDemo();
