e0fbe7ea73814fec1e0ce938ebf33bfa00bdca6c
zaxel <sae11.z.alex@gmail.com>
2024-06-21 01:00:13 +0300

    Add basic shader loader and shader path constants.

---
+++ src/const/shadersPath.js
@@ -0,0 +1,10 @@
+﻿export const basicShaderUrl = 'src/game/shaders/';
+
+export const shaderURLs = [
+    basicShaderUrl + 'grass.vert',
+    basicShaderUrl + 'grass.frag',
+];
+
+export const shadersName = [
+    'grass',
+];
